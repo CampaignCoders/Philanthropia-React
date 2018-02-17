0b2ae48499b1344d46a998e7debf598470eaaed3
petefitz4 <petefitz4@gmail.com>
2018-02-17 16:37:46 -0600

    initial commit

---
+++ philanthropia/src/components/Title/Title.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Title.css";
+
+const Title = props => (
+    <div className="title">
+        <h1>Philanthropia</h1>
+        <p>Choose a Fundraising Campaing below and donate to a good cause</p>
+    </div>
+);
+
+export default Title;
